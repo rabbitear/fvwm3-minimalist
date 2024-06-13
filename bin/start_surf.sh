daa259bf73d3681d5d6b2adb46a9dd7fb4976b7e
Jon Bradley <weatchu@gmail.com>
2024-06-12 17:08:11 -0800

    window placement of start_surf youtube xterm, up top

--- bin/start_surf.sh
+++ bin/start_surf.sh
@@ -3,7 +3,7 @@
 clipboard="$(xsel -o -b)"
 if [[ $clipboard =~ ^https://(www.)?youtu\.?be ]] then
   echo "youtube mpv: $clipboard" >> ~/.fvwm/fvwm3-output.log
-  xterm -T YouTube -font 8x16 +sb -geometry 76x2 -e mpv --geometry=640x480 --ytdl-format=mp4 --profile=fast --hwdec=auto-safe "$clipboard" &
+  xterm -T YouTube -font 8x16 +sb -geometry 76x2+259+0 -e mpv --geometry=640x480 --ytdl-format=mp4 --profile=fast --hwdec=auto-safe "$clipboard" &
 elif [[ $clipboard =~ ^https?: ]] then
   echo "clipboard = $clipboard" >> ~/.fvwm/fvwm3-output.log 
   /usr/local/bin/surf -z 1.0 "$clipboard" 2>/dev/null &
