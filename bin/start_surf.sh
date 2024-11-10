3bf4b5740e0500c21bd3efd87e04e234cfaff0e0
Jon Bradley <weatchu@gmail.com>
2024-11-10 12:22:24 -0900

    fvwm from freebsd basket

--- bin/start_surf.sh
+++ bin/start_surf.sh
@@ -1,6 +1,10 @@
 #!/usr/local/bin/bash 
 
-clipboard="$(xsel -o -b)"
+# xsel option -b is from clipboard
+#clipboard="$(xsel -o -b)"
+#clipboard="$(xclip -out -selection primary)"
+clipboard="$(xsel --output)"
+
 if [[ $clipboard =~ ^https://(www.)?youtu\.?be ]] then
   echo "youtube mpv: $clipboard" >> ~/.fvwm/fvwm3-output.log
   xterm -T YouTube -font 8x16 +sb -geometry 76x2+259+0 -e mpv --geometry=640x480 --ytdl-format=mp4 --profile=fast --hwdec=auto-safe "$clipboard" &
