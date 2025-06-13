13024e32d0848e2c4d2b2829edb8fce5ba3c98d4
Jon Bradley <weatchu@gmail.com>
2025-06-13 05:46:55 -0800

    add zoomer.sh sounds, and click xeyes to zoom

--- bin/zoomer.sh
+++ bin/zoomer.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-mpv --quiet $HOME/.fvwm/zoom-sound.ogg &
+#mpv --quiet $HOME/.fvwm/zoom-sound.ogg &
 #notify-send --expire-time=2000 "boomer on"
-~/.local/bin/boomer
 
+mpv --quiet $HOME/.fvwm/sounds/left-round-bracket
+~/.local/bin/boomer
+mpv --quiet $HOME/.fvwm/sounds/right-round-bracket
