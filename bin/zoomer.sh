0c80aeb6ce54b6263dd78c0cfe8536786668d378
Jon Bradley <weatchu@gmail.com>
2025-06-25 00:19:04 -0800

    update zoomer

--- bin/zoomer.sh
+++ bin/zoomer.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 #mpv --quiet $HOME/.fvwm/zoom-sound.ogg &
 #notify-send --expire-time=2000 "boomer on"
+if [[ -n "$(pidof boomer)" ]]; then
+    kill $(pidof boomer)
+    exit 1
+else
+    mpv --quiet $HOME/.fvwm/sounds/left-round-bracket
+    ( sleep 0.2; xdotool click 5 ) &
+    $HOME/.local/bin/boomer
 
-mpv --quiet $HOME/.fvwm/sounds/left-round-bracket
-~/.local/bin/boomer
-mpv --quiet $HOME/.fvwm/sounds/right-round-bracket
+    notify-send --expire-time=550 " --+- boomer off -+-- "
+    mpv --quiet $HOME/.fvwm/sounds/right-round-bracket
+fi
