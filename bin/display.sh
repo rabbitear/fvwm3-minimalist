2aba3c7ee74bc4882e23e11955214202ce9a823a
Jon Bradley <weatchu@gmail.com>
2025-07-04 15:50:45 -0800

    enable moving windows to edge and scrolling while dragging

--- bin/display.sh
+++ bin/display.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
+# buzzy's USB-C port seems to come up as DP-2
 if [[ $(hostname) == "buzzy" ]]
 then
     xrandr --output DP-2 --rotate normal
+    xrandr --output DP-2 --primary
 fi
+xgamma -bgamma 0.3
+
 
 #xrandr --output DSI-1 --off --output DP-1 --mode 1600x900 --primary
 
