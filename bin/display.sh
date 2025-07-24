be44dea1d39ec09ed57c396fa1cd8754e4765bf1
Jon Bradley <weatchu@gmail.com>
2025-07-23 16:46:58 -0800

    refactor(bin): Simplify display settings script

--- bin/display.sh
+++ bin/display.sh
@@ -1,24 +1,29 @@
 #!/usr/bin/bash
 
-# buzzy's USB-C port seems to come up as DP-2
 if [[ $(hostname) == "buzzy" ]]
 then
-    if xrandr | grep "DP-2 connected"
-    then
-        xrandr --output DP-2 --rotate normal --primary
-    fi
+    xrandr --output HDMI-1 --mode 1280x1024 --rate 75.02
 fi
 
-# For the 42 inch on the way, this is a good resolution size.
-if xrandr | grep "HDMI-1 connected"
-then
-    xrandr --output HDMI-1 --mode 1280x720
-fi 
-
-if xrandr | grep "DP-1 connected"
-then
-    xrandr --output DP-1 --mode 1280x1024 --below HDMI-1
-fi
+## buzzy's USB-C port seems to come up as DP-2
+#if [[ $(hostname) == "buzzy" ]]
+#then
+#    if xrandr | grep "DP-2 connected"
+#    then
+#        xrandr --output DP-2 --rotate normal --primary
+#    fi
+#fi
+#
+## For the 42 inch on the way, this is a good resolution size.
+#if xrandr | grep "HDMI-1 connected"
+#then
+#    xrandr --output HDMI-1 --mode 1280x720
+#fi 
+#
+#if xrandr | grep "DP-1 connected"
+#then
+#    xrandr --output DP-1 --mode 1280x1024 --below HDMI-1
+#fi
 
 # Take out a lot of the blue color.
 xgamma -bgamma 0.3
