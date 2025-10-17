3be00825a4ac454f0836a006258059fdf4c1a851
Jon Bradley <weatchu@gmail.com>
2025-10-17 00:51:34 -0800

    make monitor displayport 0 left rotate, fix trackpoint speed to slow

--- bin/display.sh
+++ bin/display.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-xrandr --output DSI-1 --off --output DP-1 --mode 1600x900 --primary
+#xrandr --output DSI-1 --off --output DP-1 --mode 1600x900 --primary
 
 ## For the trackpoint
-xinput set-prop 13 299 0, 0, 1
+#xinput set-prop 13 299 0, 0, 1
 
+xrandr --output "DisplayPort-0" --rotate left
 
+xinput set-prop "USB-HID Keyboard Mouse" "libinput Accel Speed" -0.2
+xinput set-prop "USB-HID Keyboard Mouse" "libinput Accel Profile Enabled" 0, 0, 1
